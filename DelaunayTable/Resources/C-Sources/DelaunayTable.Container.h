f00ecc8d4f77f0f2fd0c79822ab742e346937187
ijknabla <ijknabla@gmail.com>
2020-05-09 10:25:45 +0900

    Add HashMap methods prototype definitions

--- DelaunayTable/Resources/C-Sources/DelaunayTable.Container.h
+++ DelaunayTable/Resources/C-Sources/DelaunayTable.Container.h
@@ -77,3 +77,92 @@ static inline size_t List__size(
     }
     return size;
 }
+
+
+
+/**
+ * # Map
+ * basic data types and APIs
+ */
+typedef void* Map__key;
+typedef void* Map__value;
+
+typedef struct {
+    Map__key   key;
+    Map__value value;
+} Map__Pair;
+
+typedef Map__key Map__key__copy_function (
+    const Map__key
+);
+typedef void Map__key__delete_function (
+    Map__key
+);
+typedef size_t Map__key__hash_function (
+    const Map__key
+);
+typedef bool Map__key__equality_function (
+    const Map__key, const Map__key
+);
+
+typedef Map__value Map__value__copy_function (
+    const Map__value
+);
+typedef void Map__value__delete_function (
+    Map__value
+);
+
+
+/** # HashMap
+ * open-hash map {key => value}
+ */
+typedef struct {
+    size_t size;
+    size_t max_size;
+    List* pairLists;
+} HashMap;
+
+/// ## HashMap methods
+extern HashMap* HashMap__new(
+);
+
+extern void HashMap__clear(
+    HashMap* self,
+    Map__key__delete_function* key__delete,
+    Map__value__delete_function* value__delete
+);
+
+extern void HashMap__delete(
+    HashMap* self,
+    Map__key__delete_function* key__delete,
+    Map__value__delete_function* value__delete
+);
+
+extern bool HashMap__get(
+    const HashMap* self,
+    const Map__key key,
+    Map__value* value,
+    Map__key__hash_function* key__hash,
+    Map__key__equality_function* key__equality
+);
+
+extern int HashMap__set(
+    HashMap* self,
+    const Map__key key,
+    const Map__value value,
+    Map__key__copy_function* key__copy,
+    Map__key__delete_function* key__delete,
+    Map__key__hash_function* key__hash,
+    Map__key__equality_function* key__equality,
+    Map__value__copy_function* value__copy,
+    Map__value__delete_function* value__delete
+);
+
+extern bool HashMap__remove(
+    HashMap* self,
+    const Map__key key,
+    Map__key__delete_function* key__delete,
+    Map__key__hash_function* key__hash,
+    Map__key__equality_function* key__equality,
+    Map__value__delete_function* value__delete
+);
