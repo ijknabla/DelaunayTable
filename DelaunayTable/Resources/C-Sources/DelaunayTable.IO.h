fdbbbe4d40d12ad563a5ecd7b75e0138ca5e244d
ijknabla <ijknabla@gmail.com>
2020-05-11 21:39:54 +0900

    Add readModelicaStandardTxtTableFormatV1

--- DelaunayTable/Resources/C-Sources/DelaunayTable.IO.h
+++ DelaunayTable/Resources/C-Sources/DelaunayTable.IO.h
@@ -12,3 +12,11 @@ extern DelaunayTable* DelaunayTable__open(
 extern void DelaunayTable__close(
     DelaunayTable* this
 );
+
+extern int readModelicaStandardTxtTableFormatV1(
+    const char* tableName,
+    const char* fileName,
+    size_t* nRow,
+    size_t* nCol,
+    double** table
+);
