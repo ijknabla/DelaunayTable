9f980a05616483327ec23d184263d5a1da5c25c9
ijknabla <ijknabla@gmail.com>
2020-05-20 18:43:31 +0900

    Put double quotation

--- DelaunayTable/Resources/C-Sources/DelaunayTable.h
+++ DelaunayTable/Resources/C-Sources/DelaunayTable.h
@@ -21,9 +21,9 @@ extern _Noreturn void ModelicaFormatError(const char *string, ...);
 
 /// # macro for error
 #define MemoryError_format    "Memory allocation error at %s:%d\n"
-#define FileOpenError_format  "Can't open file %s at %s:%d\n"
-#define FileCloseError_format "Can't close file %s at %s:%d\n"
-#define FileReadError_format  "Can't read line %zu of file %s at %s:%d\n"
+#define FileOpenError_format  "Can't open file \"%s\" at %s:%d\n"
+#define FileCloseError_format "Can't close file \"%s\" at %s:%d\n"
+#define FileReadError_format  "Can't read line %zu from file \"%s\" at %s:%d\n"
 
 #define raise_MemoryError                     (ModelicaFormatError(MemoryError_format, __FILE__, __LINE__))
 #define raise_FileOpenError(fileName)         (ModelicaFormatError(FileOpenError_format, (char*) fileName, __FILE__, __LINE__))
