2710a2fad11ec3dd84a1e9f913da9dbaa9b10e21
ijknabla <ijknabla@gmail.com>
2020-06-02 16:30:41 +0900

    Add static inline compare function for double

--- DelaunayTables/Resources/C-Sources/DelaunayTable.Geometry.h
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.Geometry.h
@@ -6,6 +6,13 @@
 #include <math.h>
 
 
+/// relative tolerance for `double__compare`
+static const double rel_tol = 1.0e-9;
+
+/// absolute tolerance for `double__compare`
+static const double abs_tol = 0.0;
+
+
 static inline double double__abs(
     const double x
 ) {
@@ -17,4 +24,19 @@ static inline double double__max(
     const double y
 ) {
     return (x>y) ? x : y;
+}
+
+static inline int double__compare(
+    const double x,
+    const double y
+) {
+    const double diff = x - y;
+    const double relative_eps = double__max(
+        abs_tol,
+        rel_tol * double__max(double__abs(x), double__abs(y))
+    );
+
+    if (diff < -relative_eps) return -1;
+    if (diff > +relative_eps) return +1;
+    return 0;
 }
