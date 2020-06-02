6a7bcce20e16a3b03003c10e7911cc71be69358f
ijknabla <ijknabla@gmail.com>
2020-06-02 19:22:28 +0900

    Implement divisionRatioFromPolygonVertices(...)

--- DelaunayTables/Resources/C-Sources/DelaunayTable.Geometry.c
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.Geometry.c
@@ -124,3 +124,56 @@ finally:
 
     return status;
 }
+
+int divisionRatioFromPolygonVertices(
+    const size_t nDim,
+    const double* const* const polygon,  // double[nDim+1][nDim]
+    const double*        const point,    // double[nDim]
+          double*        const ratio     // double[nDim+1]
+) {
+    int status = SUCCESS;
+
+    double* matrix = NULL;  // double[nDim, nDim]
+    double* rel_p  = NULL;  // double[nDim]
+
+    if (!(matrix = (double*) MALLOC(nDim * nDim * sizeof(double)))) {
+        status = FAILURE; goto finally;
+    }
+    if (!(rel_p = (double*) MALLOC(nDim * sizeof(double)))) {
+        status = FAILURE; goto finally;
+    }
+
+    status = calculate_divisionRatioMatrix(nDim, polygon, matrix);
+    if (status) {goto finally;}
+
+    for (size_t i = 0 ; i < nDim ; i++) {
+        rel_p[i] = point[i] - polygon[0][i];
+    }
+
+    LAPACK__dgemv(
+        'N',        // trans
+        nDim,       // m
+        nDim,       // n
+        1.0,        // alpha
+        matrix,     // a
+        nDim,       // lda
+        rel_p,      // x
+        1,          // incx
+        0.0,        // beta
+        &ratio[1],  // y
+        1           // incy
+    );
+    if (status) {goto finally;}
+
+    ratio[0] = 1.0;
+    for(size_t i = 1 ; i < (nDim+1) ; i++) {
+        ratio[0] -= ratio[i];
+    }
+
+finally:
+
+    if (matrix) FREE(matrix);
+    if (rel_p)  FREE(rel_p);
+
+    return status;
+}
