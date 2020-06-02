f4c1bb8f2354e7a1b65f1bf476832da7e04c207e
ijknabla <ijknabla@gmail.com>
2020-06-02 16:23:43 +0900

    Add static inline functions abs & max for double

--- DelaunayTables/Resources/C-Sources/DelaunayTable.Geometry.h
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.Geometry.h
@@ -3,3 +3,18 @@
 
 #include "DelaunayTable.Common.h"
 
+#include <math.h>
+
+
+static inline double double__abs(
+    const double x
+) {
+    return fabs(x);
+}
+
+static inline double double__max(
+    const double x,
+    const double y
+) {
+    return (x>y) ? x : y;
+}
