fcf805694400f8ed752d746df795a4d6736adc8f
ijknabla <ijknabla@gmail.com>
2020-06-03 16:35:13 +0900

    Implement new, copy, delete for IndexVector

--- DelaunayTables/Resources/C-Sources/DelaunayTable.IndexVector.c
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.IndexVector.c
@@ -1,2 +1,22 @@
 
 #include "DelaunayTable.IndexVector.h"
+
+
+/// ## IndexVector methods
+IndexVector* IndexVector__new(
+    const size_t capacity
+) {
+    return Vector__new(capacity, sizeof(size_t));
+}
+
+IndexVector* IndexVector__copy(
+    const IndexVector* const this
+) {
+    return Vector__copy(this, sizeof(size_t));
+}
+
+extern void IndexVector__delete(
+    IndexVector* const this
+) {
+    Vector__delete(this);
+}
