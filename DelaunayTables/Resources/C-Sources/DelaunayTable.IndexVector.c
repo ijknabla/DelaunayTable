f04f1f4b9b268e96b287c614aafeaee1f97fd866
ijknabla <ijknabla@gmail.com>
2020-06-03 16:42:23 +0900

    Implement equality, hash method of IndexVector

--- DelaunayTables/Resources/C-Sources/DelaunayTable.IndexVector.c
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.IndexVector.c
@@ -20,3 +20,31 @@ extern void IndexVector__delete(
 ) {
     Vector__delete(this);
 }
+
+bool IndexVector__equality(
+    const IndexVector* const v0,
+    const IndexVector* const v1
+) {
+    if (v0->size != v1->size) {
+        return false;
+    }
+
+    for (size_t i = 0 ; i < (v0->size) ; i++) {
+        if (IndexVector__elements(v0)[i] != IndexVector__elements(v1)[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+size_t IndexVector__hash(
+    const IndexVector* const this
+) {
+    size_t hash = 0;
+    for (size_t i = 0 ; i < (this->size) ; i++) {
+        // 11, 13 is prime number
+        hash += 11 * hash + 13 * IndexVector__elements(this)[i];
+    }
+    return hash;
+}
