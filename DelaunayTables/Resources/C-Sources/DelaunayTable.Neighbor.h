24c97bbcf1e5b433a9b255e71a27cb3f416b36d6
ijknabla <ijknabla@gmail.com>
2020-06-03 20:37:48 +0900

    Add Neighbor type

--- DelaunayTables/Resources/C-Sources/DelaunayTable.Neighbor.h
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.Neighbor.h
@@ -2,3 +2,12 @@
 #pragma once
 
 #include "DelaunayTable.IndexVector.h"
+#include "DelaunayTable.PolygonTree.h"
+
+#include <stddef.h>
+
+
+typedef struct {
+    size_t opposite;
+    PolygonTree* polygon;
+} Neighbor;
