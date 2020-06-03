b3bf8bd5698885bcbb10dae41d270557c723ed04
ijknabla <ijknabla@gmail.com>
2020-06-03 13:02:09 +0900

    Implement new & delete of PolygonTree & PolygonTreeVector

--- DelaunayTables/Resources/C-Sources/DelaunayTable.PolygonTree.c
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.PolygonTree.c
@@ -1,2 +1,56 @@
 
 #include "DelaunayTable.PolygonTree.h"
+
+
+/// ## PolygonTree methods
+PolygonTree* PolygonTree__new(
+    const size_t nDim
+) {
+    PolygonTree* const this = (PolygonTree*) MALLOC(sizeof(PolygonTree));
+    if (!this) {goto error;}
+
+    this->vertices = NULL;
+    this->children = NULL;
+
+    this->vertices = (size_t*) CALLOC(nVerticesInPolygon(nDim), sizeof(size_t));
+    if (!(this->vertices)) {goto error;}
+
+    this->children = PolygonTreeVector__new();
+    if (!(this->children)) {goto error;}
+
+    return this;
+
+error:
+
+    if (this) {
+        if (this->vertices) FREE(this->vertices);
+        if (this->children) PolygonTreeVector__delete(this->children);
+        FREE(this);
+    }
+
+    return NULL;
+}
+
+void PolygonTree__delete(
+    PolygonTree* const this
+) {
+    FREE(this->vertices);
+    PolygonTreeVector__delete(this->children);
+    FREE(this);
+}
+
+
+/// ## PolygonTreeVector methods
+PolygonTreeVector* PolygonTreeVector__new(
+) {
+    return (PolygonTreeVector*) Vector__new(
+        2,
+        sizeof(PolygonTree*)
+    );
+}
+
+void PolygonTreeVector__delete(
+    PolygonTreeVector* const this
+) {
+    Vector__delete(this);
+}
