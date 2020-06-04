6b75bf5277693def42d653019ddfa65993f2909f
ijknabla <ijknabla@gmail.com>
2020-06-04 16:22:57 +0900

    Add prototype declarations to implement

--- DelaunayTables/Resources/C-Sources/DelaunayTable.h
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.h
@@ -1,7 +1,40 @@
 
 #pragma once
 
-#include "DelaunayTable.Common.h"
+#include "DelaunayTable.PolygonTree.h"
+
+#include <stddef.h>
 
 
-typedef struct{} DelaunayTable;
+/// # DelaunayTable
+typedef struct{
+    size_t nPoints;
+    size_t nIn;
+    size_t nOut;
+    double* table;
+    double* table_extended;
+    PolygonTreeVector* polygonTreeVector;
+    NeighborPairMap*   neighborPairMap;
+} DelaunayTable;
+
+
+/// ## DelaunayTable methods
+extern int DelaunayTable__from_buffer(
+    DelaunayTable** reference,
+    const size_t nPoints,
+    const size_t nIn,
+    const size_t nOut,
+    double* buffer
+);
+
+extern void DelaunayTable__delete(
+    DelaunayTable* this
+);
+
+extern int DelaunayTable__get_value(
+    DelaunayTable** this,
+    size_t nIn,
+    size_t nOut,
+    const double* u,
+          double* y
+);
