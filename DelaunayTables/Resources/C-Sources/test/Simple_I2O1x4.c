4c00564d08eb52a1f91ee11be4a2d6ba769813a3
ijknabla <ijknabla@gmail.com>
2020-06-04 20:33:43 +0900

    Add testcode for get_value

--- DelaunayTables/Resources/C-Sources/test/Simple_I2O1x4.c
+++ DelaunayTables/Resources/C-Sources/test/Simple_I2O1x4.c
@@ -1,6 +1,7 @@
 
 #include "DelaunayTable.h"
 
+#include <stddef.h>
 #include <assert.h>
 
 
@@ -9,6 +10,19 @@
 static const int nIn = 2;
 static const int nOut = 1;
 static const int nPoints = 4;
+
+static const size_t N = 64+1;
+
+static const double x_min = -1.0;
+static const double x_max = +1.0;
+
+static inline double range(
+    const size_t i,
+    const size_t N
+) {
+    double r = (double) i / (double) (N-1);
+    return (1-r) * x_min + r * x_max;
+}
 
 static const double table[] = {
     -1, -1, u2y(-1, -1),
@@ -23,6 +37,15 @@ int main(int argc, char** argv) {
 
     assert( DelaunayTable__from_buffer(&delaunayTable, nPoints, nIn, nOut, table) == 0 );
     assert( delaunayTable != NULL );
+
+    for (size_t ix = 0 ; ix < N ; ix++)
+    for (size_t iy = 0 ; iy < N ; iy++) {
+        const double u[nIn] = {range(ix, N), range(iy, N)};
+        double y[nOut];
+
+        assert( DelaunayTable__get_value(delaunayTable, nIn, nOut, u, y) == 0 );
+        assert( double__compare(y[0], u2y(u[0], u[1])) == 0 );
+    }
 
     DelaunayTable__delete(delaunayTable);
 
