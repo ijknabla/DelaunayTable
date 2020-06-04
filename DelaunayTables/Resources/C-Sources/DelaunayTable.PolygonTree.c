8c1f731ac52b9b88a6ccade41a027a7d583aba02
ijknabla <ijknabla@gmail.com>
2020-06-04 10:50:54 +0900

    Implement PolygonTree__find(...)

--- DelaunayTables/Resources/C-Sources/DelaunayTable.PolygonTree.c
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.PolygonTree.c
@@ -46,6 +46,77 @@ int PolygonTree__append_child(
     return PolygonTreeVector__append(this->children, child);
 }
 
+int PolygonTree__find(
+    const size_t nDim,
+    PolygonTree* const root_polygon,
+    const double* const coordinates,
+    const Points points,
+    Points__get_coordinates* const get_coordinates,
+    PolygonTree** const found_polygon,
+    double* const divisionRatio
+) {
+    int status = SUCCESS;
+    *found_polygon = NULL;
+
+    const double** const shape = (const double**) MALLOC(
+        nVerticesInPolygon(nDim) * sizeof(double*)
+    );
+    if (!shape) {status = FAILURE; goto finally;}
+
+    for (size_t i = 0 ; i < nVerticesInPolygon(nDim) ; i++) {
+        shape[i] = get_coordinates(points, root_polygon->vertices[i]);
+    }
+
+    status = divisionRatioFromPolygonVertices(
+        nDim,
+        shape,
+        coordinates,
+        divisionRatio
+    );
+    if (status) {goto finally;}
+
+    // if coordinates not in root_polygon: SUCCESS, result is NULL
+    if (!divisionRatio__inside(nDim, divisionRatio)) {
+        status = SUCCESS; goto finally;
+    }
+
+    // ** coordinates in root_polygon **
+
+    // if root_polygon does not have children: SUCCESS, result is root_polygon
+    if (PolygonTree__nChildren(root_polygon) == 0) {
+        *found_polygon = root_polygon;
+        status = SUCCESS; goto finally;
+    }
+
+    // ** root_polygon has children **
+
+    // if any find(child, ...) success: SUCCESS, return first result
+    for (size_t i = 0 ; i < PolygonTree__nChildren(root_polygon) ; i++) {
+        status = PolygonTree__find(
+            nDim,
+            PolygonTree__children(root_polygon)[i],
+            coordinates,
+            points,
+            get_coordinates,
+            found_polygon,
+            divisionRatio
+        );
+        if (status) {goto finally;}
+
+        if (*found_polygon) {goto finally;}
+    }
+
+    // else: failure
+    *found_polygon = NULL;
+    status = FAILURE; goto finally;
+
+finally:
+
+    if (shape) {FREE(shape);}
+
+    return status;
+}
+
 
 /// ## PolygonTreeVector methods
 PolygonTreeVector* PolygonTreeVector__new(
