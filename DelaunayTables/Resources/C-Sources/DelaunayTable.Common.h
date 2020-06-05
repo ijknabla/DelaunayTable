0b391aa028852e53b230f292f2fa0871a1d94e28
ijknabla <ijknabla@gmail.com>
2020-06-05 11:58:34 +0900

    Implement message, error function
    if NoModelicaStdlib:
        use `fprintf` & `exit`
    else:  # libModelicaExternalC exists in environment
        use `ModelicaFormatMessage` & `ModelicaFormatError`

--- DelaunayTables/Resources/C-Sources/DelaunayTable.Common.h
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.Common.h
@@ -2,6 +2,8 @@
 #pragma once
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 
 #define MALLOC  (malloc)
@@ -12,3 +14,47 @@
 
 #define SUCCESS ( 0)
 #define FAILURE (-1)
+
+
+#if !defined(NoModelicaStdLib)
+
+#define Runtime__send_message (ModelicaFormatMessage)
+#define Runtime__send_error   (ModelicaFormatError)
+
+#else /* NoModelicaStdLib */
+
+#include <stdarg.h>
+#include <stdnoreturn.h>
+
+          static inline void Runtime__send_message(const char*, ...);
+_Noreturn static inline void Runtime__send_error  (const char*, ...);
+
+/// Alternative to `ModelicaFormatMessage`
+static inline void Runtime__send_message(
+    const char* const format, ...
+) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end  (args);
+
+    // `ModelicaFormatMessage` feeds line on each call
+    fprintf(stderr, "\n");
+}
+
+/// Alternative to `ModelicaFormatError`
+_Noreturn static inline void Runtime__send_error(
+    const char* const format, ...
+) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end  (args);
+
+    // `ModelicaFormatError` feeds line on each call
+    fprintf(stderr, "\n");
+
+    exit(EXIT_FAILURE);
+}
+
+#endif  /* !defined(NoModelicaStdLib) */
