0776871150e38d5120fcefb619c9947fcfe6337b
ijknabla <ijknabla@gmail.com>
2020-06-08 12:29:16 +0900

    Implement basic resource data types

--- DelaunayTables/Resources/C-Sources/DelaunayTable.ResourceStack.h
+++ DelaunayTables/Resources/C-Sources/DelaunayTable.ResourceStack.h
@@ -2,3 +2,13 @@
 #pragma once
 
 #include "DelaunayTable.Container.h"
+
+
+/// # basic data type for Resource, deleter
+typedef void* Resource;
+typedef void  Resource__deleter(Resource);
+
+typedef struct {
+    Resource resource;
+    Resource__deleter* deleter;
+} ResourceAndDeleter;
